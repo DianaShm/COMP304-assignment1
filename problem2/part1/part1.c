2a7a0fbdc405d5f882d7761b4e5650458e9d81de
Diana Shmoury <dshmoury22@ku.edu.tr>
2022-10-24 00:27:07 +0300

    completed part 1 of problem 2

--- problem2/part1/part1.c
+++ problem2/part1/part1.c
@@ -21,8 +21,10 @@ int main(void) {
   pid_t cpid;
   int result;
   int a, b;
+  // make 2 pipes
   mkfifo(myfifo1, 0666);
   mkfifo(myfifo2, 0666);
+  // create 2 processes
   for (i = 0; i < 2; ++i) {
     pid[i] = fork();
     if (pid[i] == 0) {
@@ -35,15 +37,17 @@ int main(void) {
     fd1 = open(myfifo1, O_RDWR);
     fd2 = open(myfifo2, O_RDWR);
     scanf("%d", &x); // reads an integer from standard input to x;
-    write(fd1, &x, sizeof(int));
-    write(fd2, &x, sizeof(int));
-    read(fd1, &a, sizeof(int));
-    read(fd2, &b, sizeof(int));
+    write(fd1, &x, sizeof(int)); // writes to the first pipe
+    write(fd2, &x, sizeof(int)); // writes to the second pipe
+    read(fd1, &a, sizeof(int));  // read from the first pipe
+    read(fd2, &b, sizeof(int));  // read from the second pipe
     printf("The final sum: %d \n", a + b);
+    // kill both processes
     kill(pid[0], SIGTERM);
     kill(pid[1], SIGTERM);
     close(fd1); close(fd2);
   } else {
+    // check if its the first or second child in order to calculate accordingly 
     fd1 = child == 1 ? open(myfifo1, O_RDWR) : open(myfifo2, O_RDWR);
     read(fd1, &x, sizeof(int));
     if (child == 1) {
