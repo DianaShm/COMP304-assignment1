aea3f847dab818b8fdcedb277c6ba5aa39305d9b
Diana Shmoury <dshmoury22@ku.edu.tr>
2022-10-23 23:43:10 +0300

    added some comments

--- problem1/part1.c
+++ problem1/part1.c
@@ -13,15 +13,17 @@ int main(void) {
   time_t curtime;
   char buffer[30];
   if (pid == 0) {
+    // child processes will print the time every second
     while (1) { //infinite loop
       sleep(1);
       gettimeofday(&tv, NULL); 
       curtime = tv.tv_sec;
-      strftime(buffer, 30, "%m-%d-%Y  %T.", localtime(&curtime));
+      strftime(buffer, 30, "%m-%d-%Y  %T.", localtime(&curtime)); 
       pid = getpid();
       printf("time: %s%ld , pid: %d \n", buffer, tv.tv_usec, pid);
     } 
   } else {
+    // parent process will sleep for 5 second and kill the child
     sleep(5);
     kill(pid, SIGKILL);
     printf("Child %d killed \n", pid);
